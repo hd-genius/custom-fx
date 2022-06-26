0f7ba6f1facbf5405ae58021feb18a650c596f09
Roy Stewart <roythomasstewart@gmail.com>
2022-06-26 12:08:59 -0400

    Added initial code

---
+++ src/main/java/me/hdenius/customfx/CustomComponent.java
@@ -0,0 +1,22 @@
+package me.hdenius.customfx;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.Pane;
+
+import java.io.IOException;
+
+public abstract class CustomComponent extends Pane {
+    protected CustomComponent() {
+        final Class<? extends CustomComponent> componentClass = this.getClass();
+        final ComponentView annotation = componentClass.getAnnotation(ComponentView.class);
+        final FXMLLoader fxmlLoader = new FXMLLoader(componentClass.getResource(annotation.value()));
+        fxmlLoader.setController(this);
+        fxmlLoader.setRoot(this);
+
+        try {
+            fxmlLoader.load();
+        } catch (IOException exception) {
+            throw new RuntimeException(exception);
+        }
+    }
+}
