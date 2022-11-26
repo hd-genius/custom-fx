489336a7ec56bf2dfe645143447f9df9abe5e171
Roy Stewart <roythomasstewart@gmail.com>
2022-11-25 21:21:31 -0500

    Added a test for calling methods from the view fxml

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,4 +4,6 @@ module me.hdgenius.customfx {
     requires java.logging;
 
     exports me.hdgenius.customfx;
+
+    opens me.hdgenius.customfx to javafx.fxml;
 }
--- src/test/java/me/hdgenius/customfx/CustomComponentTest.java
+++ src/test/java/me/hdgenius/customfx/CustomComponentTest.java
@@ -1,8 +1,11 @@
 package me.hdgenius.customfx;
 
 import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 
@@ -13,10 +16,28 @@ public class CustomComponentTest {
     }
 
     @Test
+    @DisplayName("test that the view FXML content is attached under the component")
     public void testThatViewIsLoaded() {
         final CustomComponent instance = new CustomComponentImpl();
         Assertions.assertTrue(instance.getChildren().size() > 0);
     }
+
+    @Test
+    @DisplayName("test that component methods can be called from the view's FXML")
+    public void testMethodsFromFxml() {
+        final CustomComponent instance = new CustomComponentImpl();
+        final Button button = instance.getChildren().stream()
+                .filter(child -> child instanceof Button)
+                .findFirst()
+                .map(child -> (Button)child)
+                .orElseThrow();
+        button.getOnMouseClicked().handle(null);
+        Assertions.assertTrue(true);
+    }
+
+    @Test
+    @DisplayName("test that the component can be rendered from an FXML file")
+    public void testComponentRenderedInFxml() {}
 }
 
 @ComponentView("/test-component.fxml")
@@ -24,4 +45,7 @@ class CustomComponentImpl extends CustomComponent {
     CustomComponentImpl() {
         super();
     }
+
+    @FXML
+    public void onClick() {}
 }
