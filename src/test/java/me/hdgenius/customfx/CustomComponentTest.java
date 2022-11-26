75420b8743de2725a25535ec62e3270379936a28
Roy Stewart <roythomasstewart@gmail.com>
2022-11-26 02:07:40 -0500

    Removed unused import

--- src/test/java/me/hdgenius/customfx/CustomComponentTest.java
+++ src/test/java/me/hdgenius/customfx/CustomComponentTest.java
@@ -1,7 +1,6 @@
 package me.hdgenius.customfx;
 
 import javafx.application.Platform;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
