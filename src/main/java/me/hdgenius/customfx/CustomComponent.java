866ea3a3ee25c827c71a690fc07655bb73f3b6ce
Roy Stewart <roythomasstewart@gmail.com>
2022-11-26 02:53:18 -0500

    Refactored the CustomComponent class

--- src/main/java/me/hdgenius/customfx/CustomComponent.java
+++ src/main/java/me/hdgenius/customfx/CustomComponent.java
@@ -11,24 +11,6 @@ public abstract class CustomComponent extends Pane {
         loadContent(getContentResource());
     }
 
-    private URL getContentResource() {
-        final ComponentView annotation = getClass().getAnnotation(ComponentView.class);
-        if (annotation == null) {
-            throw new ConfigurationException("No ComponentView annotation was found for the class" + getClass().getCanonicalName());
-        }
-        final String contentPath = annotation.value();
-        final URL resource = getClass().getResource(contentPath);
-
-        if (resource == null) {
-            throw new ConfigurationException(String.format(
-                    "The view resource, %s, does not exist for the component %s",
-                    contentPath,
-                    getClass().getCanonicalName()));
-        }
-
-        return resource;
-    }
-
     private void loadContent(final URL resource) {
         final FXMLLoader fxmlLoader = new FXMLLoader(resource);
         fxmlLoader.setController(this);
@@ -37,7 +19,29 @@ public abstract class CustomComponent extends Pane {
         try {
             fxmlLoader.load();
         } catch (final IOException exception) {
-            throw new InitializationException(exception, String.format("Encountered an IOException while trying to load the content for %s", getClass().getName()));
+            final String errorMessage = String.format("Encountered an IOException while trying to load the content for %s", getClass().getName());
+            throw new InitializationException(exception, errorMessage);
         }
+    }
+
+    private URL getContentResource() {
+        final String contentPath = getContentPath();
+        final URL resource = getClass().getResource(contentPath);
+
+        if (resource == null) {
+            final String errorMessage = String.format("The view resource, %s, does not exist for the component %s", contentPath, getClass().getCanonicalName());
+            throw new ConfigurationException(errorMessage);
+        }
+
+        return resource;
+    }
+
+    private String getContentPath() {
+        final ComponentView annotation = getClass().getAnnotation(ComponentView.class);
+        if (annotation == null) {
+            final String errorMessage = String.format("No ComponentView annotation was found for the class %s", getClass().getCanonicalName());
+            throw new ConfigurationException(errorMessage);
+        }
+        return annotation.value();
     }
 }
